d1acb7a67ba5f504f00cbc728890d268bffbd86c
ItachiUchiha976 <fredsoule976@gmail.com>
2026-07-09 01:49:20 +0200

    fix: détecter aussi paniers one-page

--- bos-stripe.js
+++ bos-stripe.js
@@ -118,7 +118,7 @@
   }
 
   function addStripeButton(productKey) {
-    var isCart = location.pathname.indexOf('panier') !== -1;
+    var isCart = location.pathname.indexOf('panier') !== -1 || !!document.getElementById('cartFooter');
     var link = productKey ? (STRIPE_LINKS[productKey] || null) : null;
     if (!link && !isCart) return;
 
@@ -181,7 +181,7 @@
   }
 
   function init() {
-    var isCart = location.pathname.indexOf('panier') !== -1;
+    var isCart = location.pathname.indexOf('panier') !== -1 || !!document.getElementById('cartFooter');
     if (isCart) {
       addStripeButton(null);
     } else {
